b8d42140eabec1c886a09c752d4a7fb8408c3dd0
barussa <pedro.bbaruselli@gmail.com>
2019-08-11 12:48:46 -0300

    Forca

---
+++ forca.py
@@ -0,0 +1,40 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "BANANA".upper()
+    letras_acertadas = ["_" for letras in palavra_secreta]
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+    while(not enforcou and not acertou):
+
+        chute = input("Qual a letra? ")
+        chute = chute.strip().upper()
+
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("Você tem {} tentativas restantes.".format(6-erros))
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if acertou:
+        print("Você ganhou, parabéns!")
+    else:
+        print("Você perdeu! :'(")
+    print("Fim do jogo")
+
+
+if(__name__ == "__main__"):
+    jogar()
